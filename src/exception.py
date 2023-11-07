c0962cf7498413fd7f96bda9a9f9cc1e274c12cd
moadams847 <moadams847@gmail.com>
2023-11-07 15:34:45 +0000

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys 
+import logging
+
+def error_message_details(error, error_details:sys):
+    _,_,exe_tb = error_details.exc_info()
+    file_name=exe_tb.tb_frame.f_code.co_filename
+    line_number = exe_tb.tb_lineno 
+
+    error_message = f"Error occured in python script {file_name} line number {line_number} error message {str(error)}"
+    return error_message
+
+    
+class CustomeException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+
