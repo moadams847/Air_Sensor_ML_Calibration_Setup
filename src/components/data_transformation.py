53831ab0a390b02b01d3bd180ec3e76faf95aec8
moadams847 <moadams847@gmail.com>
2023-11-21 16:48:51 +0000

    data ingestion

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,12 +1,17 @@
-import os
 import sys
-from src.exception import  CustomeException
-from src.exception import logging
-import pandas as pd
-
-from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
-class DataIngestionConfig:
-    pass
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessors_obj_file_path = os.path.join('artifacts', '[processor.pkl')
