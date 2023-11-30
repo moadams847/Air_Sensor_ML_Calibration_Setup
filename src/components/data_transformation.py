d7cb6fc029d0ab8afdcda14e846de1976ff39011
moadams847 <moadams847@gmail.com>
2023-11-30 18:51:25 +0000

    data transformation implemented

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -12,18 +12,127 @@ from src.exception import CustomException
 from src.logger import logging
 import os
 
+from src.utils import save_object
+
 @dataclass
 class DataTransformationConfig:
-    preprocessors_obj_file_path = os.path.join('artifacts', 'processor.pkl')
+    preprocessor_obj_file_path = os.path.join('artifacts', 'processor.pkl')
+
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config = DataTransformation()
+        self.data_transformation_config=DataTransformationConfig()
+
 
     def get_data_transformers(self):
+        '''
+
+        This function is responsible for data transformation
+
+        '''
         try:
-            pass
+            numerical_columns = ['PM2_5', 'Hum', 'Temp']
 
-        except:
-            pass
+            # [] #I don't have any
+            # categorical_columns = categorical_columns
 
+            num_pipeline= Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+
+                ]
+            )
+
+            # cat_pipeline=Pipeline(
+
+            #     steps=[
+            #     ("imputer",SimpleImputer(strategy="most_frequent")),
+            #     ("one_hot_encoder",OneHotEncoder()),
+            #     ("scaler",StandardScaler(with_mean=False))
+            #     ]
+
+            # )
+
+            # logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                # ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ]
+
+
+            )
+
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+
+    def initiate_data_transformation(self, train_path, test_path):
+
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed successfully")
+            logging.info("Obtaining preprocessing objects")
+
+            preprocessing_obj = self.get_data_transformers()
+
+            drop_from_df = ['DataDate', 'PM2.5']
+            target_column_name = "PM2.5"
+            numerical_columns = ['PM2_5', 'RH', 'Temp']
+
+            input_feature_train_df = train_df.drop(columns=drop_from_df, axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=drop_from_df, axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info( f"Applying preprocessing object on training dataframe and testing dataframe.")
+            
+        
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+
+            test_arr = np.c_[
+                input_feature_test_arr, np.array(target_feature_test_df)
+                ]
+
+            logging.info(f"Saved preprocessing object")
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+            
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+                    
+           
+
+
+            
+ 
