ee182560d1e8e5ca99e5a2846d7c65fc67480850
moadams847 <moadams847@gmail.com>
2023-12-04 17:47:39 +0000

    Implemented Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -56,10 +56,80 @@ class ModelTrainer:
             "CatBoosting Regressor": CatBoostRegressor(verbose=False),
             'AdaBoost Regressor':AdaBoostRegressor()
             }
+
+
+            params={
+
+                 # Linear Regression
+                "Linear Regression":{},
+
+                # Ridge Regression
+                'Ridge Regression': {
+                    'alpha': [0.001, 0.01, 0.1, 1, 10, 100] 
+                },                
             
+                # Lasso Regression
+                'Lasso Regression': {
+                    'alpha': [0.001, 0.01, 0.1, 1, 10, 100] 
+                },
+
+                # k-Nearest Neighbors
+                 'k-Nearest Neighbors': {
+                 'n_neighbors': [3, 5, 7, 9, 11],  # Number of neighbors to consider
+                 'weights': ['uniform', 'distance'],  # Weight function used in prediction
+                 'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+
+                #Support Vector Machines
+                'Support Vector Machines': {
+                'C': [0.1, 1, 10],  # Regularization parameter
+                'kernel': ['linear', 'rbf'],  # Type of kernel
+                'gamma': ['scale', 'auto', 0.1, 1]
+                },
+
+                # Random Forest
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                # Gradient Boosting
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                # XGBoost
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                # CatBoosting Regressor
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+
+                # AdaBoost Regressor
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
             model_report:dict=evaluate_models(X_train = X_train, y_train = y_train, 
                                               X_test = X_test, y_test = y_test, 
-                                              models = models)
+                                              models = models, param=params)
             
 
         ## To get best model score from dict
@@ -67,7 +137,7 @@ class ModelTrainer:
 
             ## To get best model name from dict
 
-            best_model_name = list(model_report.keys())[
+            best_model_name = list(model_report.keys())[ 
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
@@ -87,6 +157,7 @@ class ModelTrainer:
             r2_square = r2_score(y_test, predicted)
             return r2_square
         
+
         except Exception as e:
             raise CustomException(e,sys)
 
